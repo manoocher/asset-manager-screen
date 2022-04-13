0a226154ac7817d8d30d8fec5823f557fe5d8206
manoocher <manoocher.t@gmail.com>
2022-04-13 05:27:56 +0300

    add screen page

---
+++ src/DeviceDetail.jsx
@@ -0,0 +1,10 @@
+import { Box, Grid } from "@mui/system";
+import Grid from "@mui/material/Grid";
+
+export const DeviceDetail = () => {
+  return (
+    <Box>
+      <Grid container spacing={2}></Grid>
+    </Box>
+  );
+};
